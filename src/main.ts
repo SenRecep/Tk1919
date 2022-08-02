6f55e38665c101539cb3d15de56458d6055b3504
Recep <recepsen@taptoweb.com>
2022-08-02 09:45:18 +0300

    Init

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { Logger } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+
+async function bootstrap() {
+  const logger = new Logger('main');
+  const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  const PORT = configService.get<number>('PORT');
+  await app.listen(PORT);
+  logger.log(`Application listening on port ${PORT}`);
+}
+bootstrap();
